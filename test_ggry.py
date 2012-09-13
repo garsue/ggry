98eddadae58680691c370a9356e77ee76fddb5bf
Gar Sue <labs.garsue@gmail.com>
2012-09-13 18:28:39 +0900

    chenge arguments type

--- test_ggry.py
+++ test_ggry.py
@@ -11,18 +11,16 @@ import ggry
 class TestGtty(unittest.TestCase):
     def test_arg(self):
         parser = argparse.ArgumentParser()
-        parser.add_argument("words", help="search words")
+        parser.add_argument("words", nargs="+")
         parser.add_argument("--browser", "-b")
-        test = parser.parse_args(["spam ham egg", "-b", "w3m"])
-        self.assertEqual(test.words, "spam ham egg")
+        test = parser.parse_args("spam ham egg -b w3m".split())
+        self.assertEqual(test.words, ["spam", "ham", "egg"])
         self.assertEqual(test.browser, "w3m")
-
-    @unittest.skip("need command line args")
-    def test_parse_args(self):
-        test = gtty.parse_args()
-        self.assertEqual(test.words, [])
+        test = parser.parse_args("spam ham egg".split())
+        self.assertEqual(test.words, ["spam", "ham", "egg"])
+        self.assertIsNone(test.browser)
 
     def test_build_url(self):
         expect = "http://google.com/search?q=spam+%2B+ham"
-        test = ggry.build_url("spam + ham")
+        test = ggry.build_url(["spam", "+", "ham"])
         self.assertEqual(test, expect)
